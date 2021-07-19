b48a855fc68178d8ed67f16b21af7cd8098ff65c
unknown <kvashir@gmail.com>
2021-07-19 11:58:14 +0200

    servicio auth

---
+++ src/app/shared/model/user.ts
@@ -0,0 +1,10 @@
+export class User {
+
+  id?:string="";
+  name!:string;
+  surname!:string;
+  password!:string;
+  email!:string;
+  rol!:string;
+
+}
--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,9 +1,43 @@
+
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { User } from '../model/user';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  //pendiente private error: ErrorService
+  constructor(private fireAuth: AngularFireAuth,
+    private firestore: AngularFirestore) { }
+
+
+  async registrarUsuario$(user:User): Promise<any>{
+    try {
+      const credentials = await this.fireAuth.createUserWithEmailAndPassword(user.email,user.password);
+      user.id = credentials.user?.uid;
+      credentials.user?.updateProfile({
+        displayName: user.name +' '+user.surname
+      });
+
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  login(user:User):Promise<any>{
+    return this.fireAuth.signInWithEmailAndPassword(user.email,user.password);
+  }
+
+  setToken(id:any,token:any):void{
+    sessionStorage.setItem(id,token);
+  }
+
+  getToken():string{
+    return sessionStorage.getItem('accesToken') as string;
+  }
+
 }
